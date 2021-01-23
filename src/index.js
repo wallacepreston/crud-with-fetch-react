d990c584dd8a4f0ec20d1f82ae3addbc50d76e1c
Preston Wallace <wallace.preston@gmail.com>
2021-01-22 22:35:24 -0800

    feat(posts): add support for get all

--- src/index.js
+++ src/index.js
@@ -9,12 +9,28 @@ import './bootstrap.css';
 import './style.css';
 
 const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const resp = await fetch('https://jsonplace-univclone.herokuapp.com/posts/');
+      const data = await resp.json();
+      console.log('data: ', data);
+      setPosts(data)
+    }
+    fetchPosts();
+  }, []);
 
   return <>
     <h1>
       Posts
     </h1>
-    
+    {
+      posts.map(post => <div key={post.id}>
+          <h3>{post.title}</h3>
+          <div>{post.body}</div>
+        </div>)
+    }
   </>
 }
 
