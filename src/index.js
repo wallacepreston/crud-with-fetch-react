6818da1c2d21cade83dbb4b13708c796ebe9fedd
Preston Wallace <wallace.preston@gmail.com>
2021-01-22 22:34:30 -0800

    feat(initial): Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+
+// import {
+//   Home,
+// } from './components';
+
+import './bootstrap.css';
+import './style.css';
+
+const App = () => {
+
+  return <>
+    <h1>
+      Posts
+    </h1>
+    
+  </>
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
